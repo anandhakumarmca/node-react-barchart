818595ddd3f87907527de23df54af440eb9b22fe
AnandhaKumar <accetanandhmca@gmail.com>
2016-01-05 11:37:31 +0530

    Delete index.js

--- clientapp/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BarChart from './BarChart';
-
-const mountNode = document.getElementById("reactNode");
-
-React.render(<BarChart data={[1, 2, 3]} />, mountNode);
-window.setTimeout(() =>
-    React.render((<BarChart data={[1, 3, 5, 2]} />), mountNode),
-    0);
